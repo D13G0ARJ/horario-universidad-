4d93f845176510331e864ed2141056a626e84752
D13G0ARJ <rodrijime34@gmail.com>
2025-03-20 18:24:21 -0400

    chore:add seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,21 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            'cedula' => '12345678', // Cambia este valor por la cédula deseada
+            'name' => 'Juan Pérez', // Cambia este valor por el nombre deseado
+            'email' => 'juan@example.com', // Cambia este valor por el email deseado
+            'password' => Hash::make('12345678'), // Cambia este valor por la contraseña deseada
+        ]);
     }
 }
