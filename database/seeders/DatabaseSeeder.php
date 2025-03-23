7bbba781aca2d7bf2116795091521c6e512a18d3
NoSoyCrisman <cristhianb397@gmail.com>
2025-03-23 19:52:19 -0400

    New Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'cedula' => '30541863', // Cambia este valor por la cédula deseada
+            'name' => 'Cristhian Blanco', // Cambia este valor por el nombre deseado
+            'email' => 'cristhianb397@gmail.com', // Cambia este valor por el email deseado
+            'password' => Hash::make('30541863'), // Cambia este valor por la contraseña deseada
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('asignaturas')->insert([
+            'code' => 'SYC-32714', // Cambia este valor por el código de la asignatura deseada
+            'name' => 'Implantación de sistemas', // Cambia este valor por el nombre de la asignatura deseada
+        ]);
+        DB::table('carreras')->insert([
+            'code' => '2614', // Cambia este valor por el código de la carrera deseada
+            'name' => 'ING. Sistemas', // Cambia este valor por el nombre de la carrera deseada
+        ]);
     }
 }
